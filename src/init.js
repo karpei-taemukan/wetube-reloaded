587b2cd30b8f3cae483bda2af2f10e9e7438a0cd
karpei-taemukan <91212680+karpei-taemukan@users.noreply.github.com>
2022-08-06 02:25:44 +0900

    small change

--- src/init.js
+++ src/init.js
@@ -10,7 +10,6 @@ import app from "./server";
 const PORT = process.env.PORT || 4000;
 
 
-
 const handleListening = () => console.log(`Server listening on port http://localhost:${PORT}`);
 
 app.listen(PORT, handleListening);
